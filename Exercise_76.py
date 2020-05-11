257bdf64c8cc7a7d01312d02be5529d0f73effd0
xiaoming2333ca <siyi.ren21@ycdsbk12.ca>
2020-05-11 17:11:09 -0400

    Ver1

--- Exercise_76.py
+++ Exercise_76.py
@@ -1,17 +1,21 @@
-num = int(input('Please enter a integer (>=2) >'))
-count = 0
-if num < 2:
-    print("Your number is invalid")
-else:
-    print(f'The prime factor(s) of {num} is(are):')
-    factor = 2
+def PrimeNumber(num):
+    count = 0
+    if num < 2:
+        print("Your number is invalid")
+    else:
+        print(f'The prime factor(s) of {num} is(are):')
+        factor = 2
 
-    while factor <= num:
-        if num % factor == 0:
-            print(factor)
-            num = num // factor
-            count += 1
-        else:
-            factor += 1
-    if count == 1:
-        print("Your number is a prime number")
+        while factor <= num:
+            if num % factor == 0:
+                print(factor)
+                num = num // factor
+                count += 1
+            else:
+                factor += 1
+        if count == 1:
+            print("Your number is a prime number")
+
+
+n = int(input('Please enter a integer (>=2) >'))
+PrimeNumber(n)
